8606c3d36d7194cdc4ee86b94ff88f11d0ee3d2e
AdoroTutto <1019319473@qq.com>
2021-10-19 00:27:37 +0800

    docs: swagger配置完善。gateway网关项目添加，并配置网关swagger

---
+++ fog-cloud-gateway-app/src/main/java/com/ticho/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.ticho.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ *
+ * @author AdoroTutto
+ * @date 2021-10-18 22:24
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}

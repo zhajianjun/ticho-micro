f14ee5a46e1dadcc5d2265bf19a6210d5990375c
zhajianjun <1019319473@qq.com>
2022-08-08 01:19:12 +0800

    docs: 配置修改

--- ticho-storage/src/main/java/com/ticho/storage/StorageApplication.java
+++ ticho-storage/src/main/java/com/ticho/storage/StorageApplication.java
@@ -2,7 +2,6 @@ package com.ticho.storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  *
@@ -10,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @date 2021-10-06 11:10 上午
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class StorageApplication {
     public static void main(String[] args) {
         SpringApplication.run(StorageApplication.class, args);
--- ticho-uaa/src/main/java/com/ticho/uaa/UaaApplication.java
+++ ticho-uaa/src/main/java/com/ticho/uaa/UaaApplication.java
@@ -3,7 +3,6 @@ package com.ticho.uaa;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 启动器
@@ -12,7 +11,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @date 2021-10-06 11:10 上午
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 @MapperScan(basePackages = "com.ticho.uaa")
 public class UaaApplication {
     public static void main(String[] args) {

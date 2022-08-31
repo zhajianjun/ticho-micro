1cff16b78dcdc37b8238445a858cc1da9baebceb
zhajianjun <1019319473@qq.com>
2022-08-31 21:49:06 +0800

    配置调整

--- ticho-admin/src/main/java/com/ticho/admin/AdminApplication.java
+++ ticho-admin/src/main/java/com/ticho/admin/AdminApplication.java
@@ -1,5 +1,6 @@
 package com.ticho.admin;
 
+import com.ticho.boot.web.annotation.EnableTichoMvc;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @EnableAdminServer
+@EnableTichoMvc
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
